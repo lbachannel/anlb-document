12604bd937d44a8d922a5aa4b1eb3c3da3d24d52
lbachannel <anlb19749@gmail.com>
2024-08-01 17:32:39 +0700

    code chapter 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,47 @@ document.addEventListener('DOMContentLoaded', function() {
         sidebar.classList.toggle('collapsed');
     });
 });
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+function load(selector, path) {
+    const cached = localStorage.getItem(path);
+    if (cached) {
+        $(selector).innerHTML = cached;
+    }
+
+    fetch(path)
+        .then((res) => res.text())
+        .then((html) => {
+            if (html !== cached) {
+                $(selector).innerHTML = html;
+                localStorage.setItem(path, html);
+            }
+        })
+        .finally(() => {
+            window.dispatchEvent(new Event("template-loaded"));
+        });
+}
+
+window.addEventListener("template-loaded", () => {
+    const tabsSelector = "sidebar-tab__item";
+    const contentsSelector = "content";
+
+    const tabActive = `${tabsSelector}--active`;
+    const contentActive = `${contentsSelector}--active`;
+
+    const tabContainers = $$(".js-tabs");
+    tabContainers.forEach((tabContainer) => {
+        const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+        const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+        tabs.forEach((tab, index) => {
+            tab.onclick = () => {
+                tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
+                tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
+                tab.classList.add(tabActive);
+                contents[index].classList.add(contentActive);
+            };
+        });
+    });
+});
